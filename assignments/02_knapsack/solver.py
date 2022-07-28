8e331dfb3702870fe7e8fec7f982fb8d9a89d2b8
Pat WSL <pat.hearps@bluescope.com>
2022-07-28 16:53:07 +1000

    REFACT: extract input parsing

--- assignments/02_knapsack/solver.py
+++ assignments/02_knapsack/solver.py
@@ -11,22 +11,9 @@ logger = get_logger(__name__)
 Item = namedtuple("Item", ['index', 'value', 'weight'])
 
 def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
 
-    # parse the input
-    lines = input_data.split('\n')
-
-    firstLine = lines[0].split()
-    item_count = int(firstLine[0])
-    capacity = int(firstLine[1])
-
-    items = []
-
-    for i in range(1, item_count+1):
-        line = lines[i]
-        parts = line.split()
-        items.append(Item(i-1, int(parts[0]), int(parts[1])))
-
+    # parse file into useable data
+    capacity, items = parse_input(input_data)
     logger.info(f"Items:\n{pformat(items)}")
 
     # a trivial algorithm for filling the knapsack
@@ -45,6 +32,23 @@ def solve_it(input_data):
     output_data = str(value) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, taken))
     return output_data
+
+
+def parse_input(input_data):
+    # parse the input
+    lines = input_data.split('\n')
+
+    firstLine = lines[0].split()
+    item_count = int(firstLine[0])
+    capacity = int(firstLine[1])
+
+    items = []
+
+    for i in range(1, item_count+1):
+        line = lines[i]
+        parts = line.split()
+        items.append(Item(i-1, int(parts[0]), int(parts[1])))
+    return capacity, items
 
 
 if __name__ == '__main__':

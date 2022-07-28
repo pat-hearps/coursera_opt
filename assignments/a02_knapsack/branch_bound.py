f1e132cc2754a96f142e948765c239830fa621c3
Pat <pat.hearps@bluescope.com>
2022-07-28 22:38:29 +1000

    FEAT: first hack at looping through depth

--- assignments/a02_knapsack/branch_bound.py
+++ assignments/a02_knapsack/branch_bound.py
@@ -13,10 +13,32 @@ def depth_first(items: list[Item], capacity: int):
     best_value, best_weight, taken = relaxed_integer(ranked, capacity)
     logger.info(f"Best possible value with int->float relaxation: value={best_value}, weight={best_weight}")
     
-    df = pd.DataFrame(list(map(dc.asdict, taken)))
+    df = pd.DataFrame(list(map(dc.asdict, items)))
+    df = df.sort_values('density', ascending=False).reset_index(drop=True)
+
+    best_choices = [False] * len(df)  # start with nothing chosen
+    remaining_weight = capacity
+
+    for i, (itm_val, itm_wgt) in df[['value', 'weight']].iterrows():
+        best_choices[i] = True
+
+        weight_if_chosen = df.loc[best_choices, 'weight'].sum()
+        value_if_chosen = df.loc[best_choices, 'value'].sum()
+
+        if weight_if_chosen > capacity:
+            logger.debug(f"Item {i} /{itm_wgt} is too heavy, skipping")
+            best_choices[i] = False
+        else:
+            remaining_weight = capacity - weight_if_chosen
+            logger.debug(f"Remaining weight: {remaining_weight}")
+
+        logger.info(f"current:\n{df.loc[best_choices,:]}")
 
 
-    return [Item(**r) for r in df[['idx', 'value', 'weight']].to_dict('records')]
+
+
+
+    return [Item(**r) for r in df.loc[best_choices,['idx', 'value', 'weight']].to_dict('records')]
 
 
 def rank_by_density(items: list[Item]) -> list[Item]:
